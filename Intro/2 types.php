fd9a6857dc060dd4042c2957a662af7dd1878472
Jostin Rodriguez <jarzta99@gmail.com>
2024-03-06 17:19:19 -0500

    Using define() and const

---
+++ Intro/2 types.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Pepito";
+// CONSTANTS
+define("MAX_AGE", 100);
+// Same aim, sintaxis valid since PHP 5.3.0
+const USER_ID = 356;
+define("ROOT_TRANSACTIONS", [1, 2, 100, TRUE, "Hello"]);
+echo ROOT_TRANSACTIONS[4];
